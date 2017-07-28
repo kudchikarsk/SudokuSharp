9e7cc5bb469a1ef8e874f2b22c7e11cdd41fc5e6
Benjamin Chambers <benjamindchambers@outlook.com>
2017-07-28 10:59:24 -0600

    Added puzzle creatino from mask

---
+++ src/Puzzle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSharp
+{
+    class Puzzle
+    {
+        public static List<Board> CreateAll(List<int> Mask)
+        {
+            var found = new List<Board>();
+            RecurseAll(found, new Board(), Mask, 0);
+            return found;
+        }
+
+        private static void RecurseAll(List<Board> Results, Board scratch, List<int> Mask,int idx)
+        {
+            if (idx == Mask.Count)
+            {
+                if (scratch.ExistsUniqueSolution())
+                    Results.Add(new Board(scratch));
+            } else
+            {
+                foreach (var test in scratch.Find.Candidates(Mask[idx])) {
+                    scratch[Mask[idx]] = test;
+                    RecurseAll(Results, scratch, Mask, idx + 1);
+                }
+                scratch[Mask[idx]] = 0;
+            }
+        }
+    }
+}
