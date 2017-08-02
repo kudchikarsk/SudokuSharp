eb13a666832878bd6599c92782cbaacf5f2cc1c2
Benjamin Chambers <benjamindchambers@outlook.com>
2017-08-01 18:28:05 -0600

    Changed the Cut* functions to return new Boards rather than modify the current one

--- src/Board/Cut.cs
+++ src/Board/Cut.cs
@@ -8,9 +8,11 @@ namespace SudokuSharp
 {
     public partial class Board
     {
-        public void CutQuad(Random Stream)
+        public Board CutQuad(Random Stream)
         {
-            var Filled = Find.FilledLocations().ToList();
+            var work = new Board(this);
+
+            var Filled = work.Find.FilledLocations().ToList();
 
             if (Filled.Count > 0)
             {
@@ -20,24 +22,20 @@ namespace SudokuSharp
                 loc[2] = Location.FlipHorizontal(loc[0]);
                 loc[3] = Location.FlipHorizontal(loc[1]);
 
-                var test = new int[4];
-
                 for (int i = 0; i < 4; i++)
-                    test[i] = this[loc[i]];
-                for (int i = 0; i < 4; i++)
-                    this[loc[i]] = 0;
+                    work[loc[i]] = 0;
 
-                if (!ExistsUniqueSolution())
-                {
-                    for (int i = 0; i < 4; i++)
-                        this[loc[i]] = test[i];
-                }
+                if (!work.ExistsUniqueSolution())
+                    work = new Board(this);
             }
+
+            return work;
         }
 
-        public void CutPair(Random stream)
+        public Board CutPair(Random stream)
         {
-            var Filled = Find.FilledLocations().ToList();
+            var work = new Board(this);
+            var Filled = work.Find.FilledLocations().ToList();
 
             if (Filled.Count > 0)
             {
@@ -45,34 +43,33 @@ namespace SudokuSharp
                 loc[0] = Filled[stream.Next(Filled.Count)];
                 loc[1] = (stream.Next(2) == 0) ? Location.FlipVertical(loc[0]) : Location.FlipHorizontal(loc[0]);
 
-                var test = new int[2];
-
-                test[0] = this[loc[0]];
-                test[1] = this[loc[1]];
-                this[loc[0]] = 0;
-                this[loc[1]] = 0;
+                work[loc[0]] = 0;
+                work[loc[1]] = 0;
 
                 if (!ExistsUniqueSolution())
                 {
-                    this[loc[0]] = test[0];
-                    this[loc[1]] = test[1];
+                    work = new Board(this);
                 }
             }
+
+            return work;
         }
 
-        public void CutSingle(Random stream)
+        public Board CutSingle(Random stream)
         {
+            var work = new Board(this);
             var Filled = Find.FilledLocations().ToList();
 
             if (Filled.Count>0)
             {
                 var loc = Filled[stream.Next(Filled.Count)];
-                int test = this[loc];
-                this[loc] = 0;
+                work[loc] = 0;
 
                 if (!ExistsUniqueSolution())
-                    this[loc] = test;
+                    work = new Board(this);
             }
+
+            return work;
         }
     }
 }
