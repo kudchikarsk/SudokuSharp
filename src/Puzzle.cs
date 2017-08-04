e025577c1b30e49f648e6c4e3fc1c1c83b4d06ce
Benjamin Chambers <benjamindchambers@outlook.com>
2017-08-04 10:00:20 -0600

    Added a function to check if the clues provided can work as a puzzle, with specific information about why it fails.

--- src/Puzzle.cs
+++ src/Puzzle.cs
@@ -8,6 +8,63 @@ namespace SudokuSharp
 {
     public static class Puzzle
     {
+        public enum ValidityReason
+        {
+            Valid,
+            NotEnoughClues,
+            DuplicateInRow,
+            DuplicateInColumn,
+            DuplicateInZone,
+            DuplicateAtLocation
+        }
+
+        public static ValidityReason IsValid(IEnumerable<(int loc, int val)> clues)
+        {
+            if (clues.Count() < 17)
+                return ValidityReason.NotEnoughClues;
+
+            var filled = new bool[81];
+
+            var r = new bool[9];
+            var c = new bool[9];
+
+            var dr = new bool[9, 10];
+            var dc = new bool[9, 10];
+            var dz = new bool[9, 10];
+
+            foreach (var hint in clues)
+            {
+                if (filled[hint.loc])
+                    return ValidityReason.DuplicateAtLocation;
+                else
+                    filled[hint.loc] = true;
+
+                int row = Location.Row(hint.loc);
+                int col = Location.Column(hint.loc);
+                int zon = Location.Zone(hint.loc);
+
+                r[row] = true;
+                c[col] = true;
+
+                if (dr[row, hint.val])
+                    return ValidityReason.DuplicateInRow;
+                else
+                    dr[row, hint.val] = true;
+
+                if (dc[col, hint.val])
+                    return ValidityReason.DuplicateInColumn;
+                else
+                    dc[col, hint.val] = true;
+
+                if (dz[zon, hint.val])
+                    return ValidityReason.DuplicateInZone;
+                else
+                    dz[zon,hint.val] = true;
+            }
+
+            return ValidityReason.Valid;
+        }
+
         public static List<Board> CreateAll(bool[] Mask)
         {
             var newMask = from loc in Location.All
@@ -19,6 +76,9 @@ namespace SudokuSharp
 
         public static List<Board> CreateAll(List<int> Mask)
         {
+            if (Mask.Count < 17)
+                throw new ArgumentException("Not enough clues provided, a minimum of 17 is necessary.");
+
             // Check for column, row and digit representation
             var foundColumns = new bool[9];
             var foundRows = new bool[9];
