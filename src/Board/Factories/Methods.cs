881f6a8390d897ba7fe4ca348be49c5db76ebd16
Benjamin Chambers <benjamindchambers@outlook.com>
2017-08-11 13:13:19 -0600

    Moved public methods into a separate file for organization.

---
+++ src/Board/Factories/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSharp
+{
+    public partial class Board
+    {
+        public partial class PuzzleGenerator
+        {
+            public Board First()
+            {
+            }
+
+            public Board Next()
+            {
+            }
+        }
+    }
+}
