9ea7c3575769fe964867045458a71a6636026705
Benjamin Chambers <benjamindchambers@outlook.com>
2017-08-11 11:05:03 -0600

    Added a method to generate a number of puzzles

--- Tests/PuzzleGeneration/Program.cs
+++ Tests/PuzzleGeneration/Program.cs
@@ -10,25 +10,34 @@ namespace PuzzleGeneration
     {
         static void Main(string[] args)
         {
-            var mask = new List<int> { 0, 10, 20, 30, 40, 50, 60, 70, 80, 4, 14, 24, 34, 44, 36, 46, 56, 66, 76 };
+            var mask = new List<int> { 0, 10, 20, 30, 40, 50, 60, 70, 80, 4, 14, 24, 34, 44, 36, 46, 56, 66, 76, 15, 16, 25, 55, 64, 65 };
             var valuesToFill = from loc in mask
                                select (loc, 1);
             var maskedBoard = new SudokuSharp.Board().Put(valuesToFill);
 
-            Console.WriteLine("Creating a puzzle to fill:");
-            Console.WriteLine(maskedBoard);
-            Console.WriteLine();
+            Console.Write("How many puzzles should be searched for? ");
+            int n = int.Parse(Console.ReadLine());
 
-            var test = SudokuSharp.Board.CreatePuzzle(mask);
+            var start = DateTime.Now;
+            var test = SudokuSharp.Board.CreatePuzzles(mask, n);
+            var end = DateTime.Now;
 
-            Console.WriteLine("The generated puzzle:");
-            Console.WriteLine(test);
-            Console.WriteLine();
+            Console.WriteLine("Found {0} puzzles in {1} seconds, or {2} puzzles/second.", test.Count(), (end-start).TotalSeconds, test.Count()/(end-start).TotalSeconds);
 
-            Console.WriteLine("Is puzzle valid: {0}", test.IsValid);
-            Console.WriteLine("Does a single solution exist: {0}", test.ExistsUniqueSolution());
-            Console.WriteLine("Number of solutions: {0}", test.CountSolutions());
-
+            int withUnique = 0;
+            Console.WriteLine("Checking them for unique solutions...");
+            for (int i=0; i<test.Count(); i++)
+            {
+                if (i%25==0)
+                {
+                    Console.WriteLine("{0} puzzles with unique solutions found out of {1} tested.", withUnique, i);
+                }
+                if (test[i].ExistsUniqueSolution())
+                {
+                    Console.WriteLine(test[i]);
+                    withUnique++;
+                }
+            }
         }
     }
 }
--- src/Board/Factory.cs
+++ src/Board/Factory.cs
@@ -50,6 +50,43 @@ namespace SudokuSharp
             return null;
         }
 
+        public static List<Board> CreatePuzzles(List<int> LocationsToFill, int NumberOfPuzzles)
+        {
+            var result = new List<Board>();
+            PuzzlesRecurse(result, NumberOfPuzzles, LocationsToFill, new List<(int, int)>());
+            return result;
+        }
+
+        static void PuzzlesRecurse(List<Board> Result, int NumberOfPuzzles, IEnumerable<int> LocationsToFill, IEnumerable<(int loc, int val)> LocationsFilled)
+        {
+            if (LocationsToFill.Any())
+            { // Cycle through possibilities
+                var where = LocationsToFill.First();
+                var cons = new Constraints(LocationsFilled);
+                var blocking = cons.AtLocation(where);
+
+                var usedNotBlocking = from test in Enumerable.Range(1, 9)
+                                      where cons.InPuzzle[test]
+                                      where !blocking[test]
+                                      select test;
+
+                var firstNotBlocking = (from test in Enumerable.Range(1, 9)
+                                        where !cons.InPuzzle[test]
+                                        select test).FirstOrDefault();
+
+                var toTry = (firstNotBlocking > 0) ? usedNotBlocking.Concat(new[] { firstNotBlocking }) : usedNotBlocking;
+
+                foreach (var test in toTry)
+                {
+                    if (Result.Count() < NumberOfPuzzles)
+                        PuzzlesRecurse(Result, NumberOfPuzzles, LocationsToFill.Skip(1), LocationsFilled.Concat(new[] { (where, test) }));
+                }
+            }
+            else
+            { // We've filled all locations
+                Result.Add(new Board().Put(LocationsFilled));
+            }
+        }
 
     }
 }
