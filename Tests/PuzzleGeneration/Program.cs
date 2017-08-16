b96ce9b9edeac2a734152bc6b6801965a2394b97
Benjamin Chambers <benjamindchambers@outlook.com>
2017-08-16 12:22:51 -0600

    Updated PuzzleGeneration timing test

--- Tests/PuzzleGeneration/Program.cs
+++ Tests/PuzzleGeneration/Program.cs
@@ -10,34 +10,36 @@ namespace PuzzleGeneration
     {
         static void Main(string[] args)
         {
+            int numBatches = 10;
+            int batchSize = 500_000;
+
             var mask = new List<int> { 0, 10, 20, 30, 40, 50, 60, 70, 80, 4, 14, 24, 34, 44, 36, 46, 56, 66, 76, 15, 16, 25, 55, 64, 65 };
             var valuesToFill = from loc in mask
                                select (loc, 1);
             var maskedBoard = new SudokuSharp.Board().Put(valuesToFill);
 
-            Console.Write("How many puzzles should be searched for? ");
-            int n = int.Parse(Console.ReadLine());
+            var generator = new SudokuSharp.Board.PuzzleGenerator(mask);
+            var b = generator.First();
 
-            var start = DateTime.Now;
-            var test = SudokuSharp.Board.CreatePuzzles(mask, n);
-            var end = DateTime.Now;
+            int tGenerated = 0;
+            TimeSpan tSpan = new TimeSpan();
 
-            Console.WriteLine("Found {0} puzzles in {1} seconds, or {2} puzzles/second.", test.Count(), (end-start).TotalSeconds, test.Count()/(end-start).TotalSeconds);
-
-            int withUnique = 0;
-            Console.WriteLine("Checking them for unique solutions...");
-            for (int i=0; i<test.Count(); i++)
+            for (int i=0; i<numBatches;i++)
             {
-                if (i%25==0)
-                {
-                    Console.WriteLine("{0} puzzles with unique solutions found out of {1} tested.", withUnique, i);
-                }
-                if (test[i].ExistsUniqueSolution())
-                {
-                    Console.WriteLine(test[i]);
-                    withUnique++;
-                }
+                var start = DateTime.Now;
+                for (int j = 0; j < batchSize; j++)
+                    generator.Next();
+                var end = DateTime.Now;
+
+                var span = end - start;
+
+                tGenerated += batchSize;
+                tSpan += span;
+
+                Console.WriteLine("Generated {0} puzzles in {1} seconds for {2:N} puzzles/second.", batchSize, span.TotalSeconds, batchSize / span.TotalSeconds);
             }
+
+            Console.WriteLine("Generated {0} puzzles total in {1} seconds total for {2:N} puzzles/second.", tGenerated, tSpan.TotalSeconds, tGenerated / tSpan.TotalSeconds);
         }
     }
 }
